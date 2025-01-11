f3b897aa8e590b86202e5a423e90b2490a8bba00
Avazbek <qalandarovavazbek43@gmail.com>
2025-01-11 17:56:41 +0500

    doctor parolini yangilash tayyor

--- api/src/validators/passwordValidate.js
+++ api/src/validators/passwordValidate.js
@@ -1,3 +1,3 @@
 exports.passwordSchema = {
-    password: { isString: { errorMessage: "Parol string bo'lishi kerak!" }, isLength: { options: { min: 8 }, errorMessage: "Parol kamida 8 ta belgidan iborat bo'lishi kerak!" }, notEmpty: { errorMessage: "Parol talab qilinadi!" }, trim: { errorMessage: "Parol orasida ochiq joy bo'lishi mumkin emas!" } },
+    password: { isString: { errorMessage: "Parol string bo'lishi kerak!" }, isLength: { options: { min: 8 }, errorMessage: "Parol kamida 8 ta belgidan iborat bo'lishi kerak!" }, trim: { errorMessage: "Parol orasida ochiq joy bo'lishi mumkin emas!" } },
 }
