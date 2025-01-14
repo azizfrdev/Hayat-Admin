115b7895d2556860459462c62d18bb59433b8527
Avazbek <qalandarovavazbek43@gmail.com>
2025-01-14 12:42:53 +0500

    Tahlil natijasiga o'zgartirish kiritildi

--- api/src/controller/resultController.js
+++ api/src/controller/resultController.js
@@ -79,6 +79,27 @@ exports.downloadResult = async (req, res) => {
 
         doc.pipe(res)
 
+        const imagePath = path.join(__dirname, "../public/images/logo.png");
+
+        // Har bir yangi sahifani yaratish uchun umumiy funksiya
+        const createPage = () => {
+            const imageHeight = 50; // Rasm balandligi
+            const imageWidth = 150; // Rasm kengligi
+            const marginTop = 10; // Yuqori chet bo'shligi
+
+            // Sahifaning yuqorisiga rasm joylash
+            doc.image(imagePath, doc.page.width / 2 - imageWidth / 2, marginTop, {
+                width: imageWidth,
+                height: imageHeight,
+            });
+
+            // Rasm ostidan kontent boshlash
+            doc.moveDown(2); // 2 qator pastga
+        };
+
+        // Birinchi sahifani yaratish
+        createPage();
+
         doc.fillColor("#004080").fontSize(20).text('Tahlil Natijasi', { align: "center" })
 
         doc.fillColor("#000000").fontSize(16).text(`\nF.I.SH: ${result.name}`, { align: 'left' });
@@ -90,14 +111,15 @@ exports.downloadResult = async (req, res) => {
 
         const fontPath = path.join(__dirname, '../public/fonts/Roboto-VariableFont_wdth,wght.ttf');
 
-        const pageHeight = 750; // PDF sahifasining bo‘yi (standart uchun)
+        const pageHeight = doc.page.height;
+        const usableHeight = pageHeight - 80; // Rasmni e'tiborga olib, foydalaniladigan balandlik
         let yPosition = doc.y; // Hozirgi vertikal pozitsiya
 
         result.analysisResults.forEach(analysis => {
-            const textHeight = 120; // Har bir tahlil uchun taxminiy balandlik
+            const textHeight = 140; // Har bir tahlil uchun taxminiy balandlik
 
             // Agar matn sahifaga sig‘masa, yangi sahifa qo‘shish
-            if (yPosition + textHeight > pageHeight) {
+            if (yPosition + textHeight > usableHeight) {
                 doc.addPage();
                 yPosition = doc.y; // Yangi sahifa boshlanishi
             }
@@ -110,7 +132,16 @@ exports.downloadResult = async (req, res) => {
             doc.text(`Shifokor: ${analysis.doctor.uz_name}`);
             doc.text(`Lavozimi: ${analysis.doctor.uz_position}`);
             doc.text(`Shifokorning telefon raqami: ${analysis.doctor.phoneNumber}`);
+
+            doc.moveDown();
+
+            // Chiziq chizish
+            doc.moveTo(50, doc.y) // Chiziq boshlanish nuqtasi
+                .lineTo(doc.page.width - 50, doc.y) // Chiziq tugash nuqtasi
+                .stroke("#000000"); // Qora rangda chiziq chizish
+
             doc.moveDown()
+            yPosition = doc.y;
         })
 
         doc.end()
